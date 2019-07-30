780ae46a3bcdbf1ea1206db2145e0accdaf40e2e
Hadi Moshayedi <hadi@moshayedi.net>
2019-07-30 00:19:44 -0700

    remove unused code

--- example.rkt
+++ example.rkt
@@ -1,87 +1,18 @@
 #lang racket
 
-(require rsvg)
 (require pict)
 (require "dot.rkt")
 (require "digraph.rkt")
 
-(define sample_graphs
-  (list
-   "digraph D {
-     A [shape=diamond]
-     B [shape=box]
-     C [shape=circle, width=1]
-     A -> B [style=dashed, color=grey]
-     A -> C [color=\"black:invis:black\"]
-     A -> D [penwidth=5, arrowhead=none]
-   }"
-   "digraph D {
-     node [fontname=\"Arial\"];
-     node_A [shape=record    label=\"shape=record|{above|middle|below}|right\"];
-     node_B [shape=plaintext label=\"shape=plaintext|{curly|braces and|bars without}|effect\"];
-   }"
-   "digraph G {
-     a b c d;
-     subgraph cluster0 {
-       a -> b;
-       a -> c;
-       b -> d;
-       c -> d;
-     }
-     subgraph cluster1 {
-       e -> g;
-       e -> f;
-     }
-     b -> f [lhead=cluster1];
-     d -> e;
-     c -> g [ltail=cluster0,lhead=cluster1];
-     c -> e [ltail=cluster0];
-     d -> h;
-   }"
-   "digraph D {
-     A -> {B, C, D} -> {F}
-    }"
-   "digraph G {
-      subgraph cluster_0 {
-        style=filled;
-        color=lightgrey;
-        node [style=filled,color=white];
-        a0 -> a1 -> a2 -> a3;
-        label = \"process #1\";
-      }
-      subgraph cluster_1 {
-        node [style=filled];
-        b0 -> b1 -> b2 -> b3;
-        label = \"process #2\";
-        color=blue;
-      }
-      start -> a0;
-      start -> b0;
-      a1 -> b3;
-      b2 -> a3;
-      a3 -> a0;
-      a3 -> end;
-      b3 -> end;
-      start [shape=Mdiamond];
-      end [shape=Msquare];
-   }"
-   ))
-
-(define (side-by-side d)
-  (define p1 (svg-port->pict (run-dot d "svg")))
-  (define p2 (dot->pict d))
-  (hc-append 10 p1 p2))
-
-(side-by-side (first sample_graphs))
-;;(map side-by-side sample_graphs)
-
 (define d1 (make-digraph `(["a" #:shape "diamond" #:fillcolor "lightgray" #:style "filled"]
                            ["b" #:shape ,(cloud 60 30) #:label "c"]
-                           "c" "d"
+                           ["c" #:shape ,(standard-fish 100 50 #:open-mouth #t #:color "Chartreuse")
+                                #:label ""]
+                           "d"
                            "a -> b -> c"
                            "a -> d -> c"
                            (subgraph "stdout" #:style "filled" #:fillcolor "cyan"
-                                     ("f"
+                                     (["f" #:shape ,(file-icon 50 60 "bisque")]
                                       "g"
                                       "f -> g"))
                            "d -> g")))
