18a3439da454a4d904f10106bb6a67a67a7d9d62
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2019-08-01 16:19:58 +0100

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "racket-package")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/racket-package.scrbl" ())))
+(define pkg-desc "The goal of this library is to make composition of Racket Pict and Graphviz Diagrams possible.")
+(define version "0.0")
+(define pkg-authors '(pykello))
