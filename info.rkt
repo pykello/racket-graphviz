6b331ec6abfc1881ae5f3070f4769a92ac87b8a1
Hadi Moshayedi <hadi@moshayedi.net>
2019-08-09 07:06:09 -0700

    bump version

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/graphviz.scrbl" ())))
 (define pkg-desc "The goal of this library is to make composition of Racket Pict and Graphviz Diagrams possible.")
-(define version "0.0")
+(define version "0.0.1")
 (define pkg-authors '(pykello))
